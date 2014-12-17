5a56f71da5a25698d7fae96de00b0284af710974
Rollie Ma <rollie.ma@gmail.com>
2014-12-16 23:31:14 -0800

    Added an empty Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/*
+ * nuntius
+ *
+ * Web page instance messenger
+ * https://github.com/waltzofpearls/nuntius
+ *
+ * Copyright (c) 2014 Topbass Labs (topbasslabs.com)
+ * Author Waltz.of.Pearls <rollie@topbasslabs.com, rollie.ma@gmail.com>
+ */
